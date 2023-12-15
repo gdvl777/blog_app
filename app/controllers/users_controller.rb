5babe1dabd89c7f6439d066392f51c7e9a3a3eff
gdvl777 <gdvl777@users.noreply.github.com>
2023-12-15 09:27:19 -0500

    app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
-    @post = @user.posts.order(created_at: :desc).first
+    @three_most_recent_posts = @user.posts.order(created_at: :desc).limit(3)
   end
 end
