6e71e42d34bb2870f216fcfc485b08f1e87690bd
gdvl777 <gdvl777@users.noreply.github.com>
2023-12-15 16:47:23 -0500

    config

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,32 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+             controllers: {
+                 sessions: 'users/sessions',
+                 registrations: 'users/registrations'
+             }
 
   root 'users#index'
+  # resources :posts do
+  #   resources :comments
+  # end
+
+  namespace :api do
+    namespace :v1 do
+      resources :posts
+      get 'posts/:post_id/comments/' , to: 'posts#comments_for_post'
+      post 'posts/:post_id/comments/', to: 'posts#create_comment'
+    end
+  end
+
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show', as: 'user'
+
+  delete '/users/:user_id/posts/:id', to: 'posts#destroy', as: 'delete_user_post'
 
   get '/users/:user_id/posts/new', to: 'posts#new', as: 'new_user_post'
   post '/users/:user_id/posts', to: 'posts#create', as: 'user_posts_create'
 
+  delete '/users/:user_id/posts/:post_id/comments/:comment_id', to: 'comments#destroy', as: 'delete_user_post_comment'
   get '/users/:user_id/posts/:post_id/comments/new', to: 'comments#new', as: 'new_user_post_comment'
   post '/users/:user_id/posts/:post_id/comments', to: 'comments#create', as: 'user_post_comments'
 
